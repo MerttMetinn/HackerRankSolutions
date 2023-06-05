431f9e1924aa9cbdca81b6d358dc17b4b6596ef4
Mert Metin <mertmetin39@gmail.com>
2023-06-05 23:39:56 +0300

    Create Easy -Connecting Towns.c

---
+++ Mathematics/Easy -Connecting Towns.c
@@ -0,0 +1,214 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'connectingTowns' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY routes
+ */
+
+int connectingTowns(int n, int routes_count, int* routes)
+{
+    int res = 1;
+
+    for(int i=0 ; i < routes_count ; i++)
+    {
+        res *= *(routes + i);
+        res %= 1234567;
+    }
+    return res;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int t = parse_int(ltrim(rtrim(readline())));
+
+    for (int t_itr = 0; t_itr < t; t_itr++)
+    {
+        int n = parse_int(ltrim(rtrim(readline())));
+
+        char** routes_temp = split_string(rtrim(readline()));
+
+        int* routes = malloc((n - 1) * sizeof(int));
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int routes_item = parse_int(*(routes_temp + i));
+
+            *(routes + i) = routes_item;
+        }
+
+        int result = connectingTowns(n, n - 1, routes);
+
+        fprintf(fptr, "%d\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline()
+{
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true)
+    {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line)
+        {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n')
+        {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data)
+        {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n')
+    {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data)
+        {
+            data = '\0';
+        }
+    }
+    else
+    {
+        data = realloc(data, data_length + 1);
+
+        if (!data)
+        {
+            data = '\0';
+        }
+        else
+        {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str)
+{
+    if (!str)
+    {
+        return '\0';
+    }
+
+    if (!*str)
+    {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str))
+    {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str)
+{
+    if (!str)
+    {
+        return '\0';
+    }
+
+    if (!*str)
+    {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end))
+    {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str)
+{
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token)
+    {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits)
+        {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str)
+{
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0')
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
