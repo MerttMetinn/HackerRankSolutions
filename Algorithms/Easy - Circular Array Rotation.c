8fd8a3276997766ad62bad4bd8b95f74e9ae43e7
Mert Metin <mertmetin39@gmail.com>
2023-08-01 23:46:38 +0300

    Create Easy - Circular Array Rotation.c

---
+++ Algorithms/Easy - Circular Array Rotation.c
@@ -0,0 +1,269 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'circularArrayRotation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY queries
+ */
+
+/*
+ * To return the integer array from the function, you should:
+ *     - Store the size of the array to be returned in the result_count variable
+ *     - Allocate the array statically or dynamically
+ *
+ * For example,
+ * int* return_integer_array_using_static_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     static int a[5] = {1, 2, 3, 4, 5};
+ *
+ *     return a;
+ * }
+ *
+ * int* return_integer_array_using_dynamic_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     int *a = malloc(5 * sizeof(int));
+ *
+ *     for (int i = 0; i < 5; i++) {
+ *         *(a + i) = i + 1;
+ *     }
+ *
+ *     return a;
+ * }
+ *
+ */
+int* circularArrayRotation(int a_count, int* a, int k, int queries_count, int* queries, int* result_count)
+{
+    int* result = (int*) malloc(queries_count * sizeof(int));
+
+    int num_rotations = k % a_count;
+
+    for (int i = 0; i < queries_count; i++)
+    {
+        int rotated_index = (queries[i] + a_count - num_rotations) % a_count;
+
+        result[i] = a[rotated_index];
+    }
+
+    *result_count = queries_count;
+
+    return result;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char** first_multiple_input = split_string(rtrim(readline()));
+
+    int n = parse_int(*(first_multiple_input + 0));
+
+    int k = parse_int(*(first_multiple_input + 1));
+
+    int q = parse_int(*(first_multiple_input + 2));
+
+    char** a_temp = split_string(rtrim(readline()));
+
+    int* a = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+        int a_item = parse_int(*(a_temp + i));
+
+        *(a + i) = a_item;
+    }
+
+    int* queries = malloc(q * sizeof(int));
+
+    for (int i = 0; i < q; i++)
+    {
+        int queries_item = parse_int(ltrim(rtrim(readline())));
+
+        *(queries + i) = queries_item;
+    }
+
+    int result_count;
+    int* result = circularArrayRotation(n, a, k, q, queries, &result_count);
+
+    for (int i = 0; i < result_count; i++)
+    {
+        fprintf(fptr, "%d", *(result + i));
+
+        if (i != result_count - 1)
+        {
+            fprintf(fptr, "\n");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline()
+{
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true)
+    {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line)
+        {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n')
+        {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data)
+        {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n')
+    {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data)
+        {
+            data = '\0';
+        }
+    }
+    else
+    {
+        data = realloc(data, data_length + 1);
+
+        if (!data)
+        {
+            data = '\0';
+        }
+        else
+        {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str)
+{
+    if (!str)
+    {
+        return '\0';
+    }
+
+    if (!*str)
+    {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str))
+    {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str)
+{
+    if (!str)
+    {
+        return '\0';
+    }
+
+    if (!*str)
+    {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end))
+    {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str)
+{
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token)
+    {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits)
+        {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str)
+{
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0')
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
